0643aab762422982a5c923bf008de81167b8ede3
Fernando <f.sanchezvilela@gmail.com>
2018-11-04 19:34:34 +0100

    Updates

---
+++ precache-manifest.44cb825aa7ef67ca1b4e3673ffb7debd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "55a1e64a0b6c299781a1",
+    "url": "/contactmanager/static/js/main.55a1e64a.chunk.js"
+  },
+  {
+    "revision": "43296c50687da3a7297d",
+    "url": "/contactmanager/static/js/1.43296c50.chunk.js"
+  },
+  {
+    "revision": "55a1e64a0b6c299781a1",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "43296c50687da3a7297d",
+    "url": "/contactmanager/static/css/1.e0b3ee9f.chunk.css"
+  },
+  {
+    "revision": "92320bfce81a940f548052a282ce25e9",
+    "url": "/contactmanager/index.html"
+  }
+];
